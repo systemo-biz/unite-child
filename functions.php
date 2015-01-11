a6f58626ff8a656a9a9cff44ff762b88b03a3ddc
yumashev <a.yumashev@gmail.com>
2015-01-11 12:59:45 +0500

    upgrade localization

--- functions.php
+++ functions.php
@@ -1,2 +1,6 @@
 <?php
 
+function child_theme_setup_s() {
+	load_child_theme_textdomain( 'unite', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'child_theme_setup_s' );
