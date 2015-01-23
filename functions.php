e9b60615b9ac7c9237afc03c0953d593e5e11a69
yumashev <a.yumashev@gmail.com>
2015-01-23 18:22:08 +0500

    update

--- functions.php
+++ functions.php
@@ -1,5 +1,57 @@
 <?php
 
+
+
+
+// Change default "Read More" button when using the_excerpt
+function unite_excerpt_more_s( $more ) {
+  return ' <a class="btn btn-default"  href="'. get_permalink( get_the_ID() ) . '">Читать далее <i class="fa fa-chevron-right"></i></a>';
+}
+add_filter( 'excerpt_more', 'unite_excerpt_more_s', 11 );
+
+/*###############################
+Включаем иконки WP для сайта
+*/
+function my_dashicons() {
+    wp_enqueue_style( 'dashicons' );
+}
+add_action( 'wp_enqueue_scripts', 'my_dashicons' );
+
+/*###############################
+Регистрируем три сайдбара для шапки, используем в header.php
+*/
+
+register_sidebar(array(
+	'name' => __('Header 1'),
+	'id' => 'header-widget-1',
+	'description' => __('Виджеты в шапке'),
+	'before_widget' => '',
+	'after_widget' => '',
+	'before_title' => '<h3><a href="#">',
+	'after_title' => '</a></h3>',
+));
+register_sidebar(array(
+	'name' => __('Header 2'),
+	'id' => 'header-widget-2',
+	'description' => __('Виджеты в шапке'),
+	'before_widget' => '',
+	'after_widget' => '',
+	'before_title' => '<h3><a href="#">',
+	'after_title' => '</a></h3>',
+));
+register_sidebar(array(
+	'name' => __('Header 3'),
+	'id' => 'header-widget-3',
+	'description' => __('Виджеты в шапке'),
+	'before_widget' => '',
+	'after_widget' => '',
+	'before_title' => '<h3><a href="#">',
+	'after_title' => '</a></h3>',
+));
+
+/*###########################
+Подгрузка локализации из дочерней темы
+*/
 function child_theme_setup_s() {
 	load_child_theme_textdomain( 'unite', get_stylesheet_directory() . '/languages' );
 }
---
+++ page-fullwidth-notitle.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Full-width (no sidebar, no title)
+ *
+ * This is the template that displays full width page without sidebar
+ *
+ * @package unite
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area col-sm-12 col-md-12">
+		<main id="main" class="site-main" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                        <?php
+                            wp_link_pages( array(
+                                'before' => '<div class="page-links">' . __( 'Pages:', 'unite' ),
+                                'after'  => '</div>',
+                            ) );
+                        ?>
+                    </div><!-- .entry-content -->
+                    <?php edit_post_link( __( 'Edit', 'unite' ), '<footer class="entry-meta"><i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span></footer>' ); ?>
+                </article><!-- #post-## -->
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
